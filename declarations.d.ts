84cef5ca9fda324ad5757ea126fa0b8917135ef1
najathi <najathi777@gmail.com>
2020-06-18 14:54:36 +0530

    update declarations.d.ts

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,10 +1,4 @@
-// declaration.d.ts
 declare module '*.scss' {
     const content: { [styleName: string]: string };
     export default content;
 }
-
-// declare module "*.svg" {
-//     const content: any;
-//     export default content;
-// }
